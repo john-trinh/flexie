d53bfe68a6ad4782e61ae2d70703e860c31f1688
Nguyen.tlc <nguyen.tlc@sutrixsolutions.com>
2018-08-04 16:18:18 +0700

    search function

--- Components/SearchBarComponent.js
+++ Components/SearchBarComponent.js
@@ -1,30 +1,67 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import { StyleSheet, View, Button, Platform, TextInput } from 'react-native';
+// import { SearchBar } from 'react-native-elements';
 import { Constants } from 'expo';
+import { StackNavigator } from 'react-navigation';
 
 export default class SearchBarComponent extends Component {
   constructor(props) {
     super(props);
     this.searchMovie = React.createRef();
+    this.state ={
+      search: ''
+    };
   }
 
-  componentDidMount(){
-    this.searchMovie.focus();
+  handleChangeText(text) {
+    this.setState({
+      search: text
+    });
   }
+
+  handleSearch() {
+    if (this.state.search.length > 0) {
+      this.searchMovie.blur();
+      this.props.searchText(this.state.search);
+    }
+  }
+
   render() {
     return (
       <View style={style.header}>
-        <SearchBar
+        <TextInput
           placeholder='Type Here...'
+          autoFocus={true}
+          onChangeText={this.handleChangeText.bind(this)}
           ref={(search) => this.searchMovie = search}
+          onSubmitEditing={ () => this.handleSearch()}
+          selectTextOnFocus={true}
+          style={style.bar}
+          underlineColorAndroid="#fff"
         />
+        <Button onPress={this.handleSearch.bind(this)} title='Search' style={style.button}/>
       </View>
     )
   }
 }
 const style = StyleSheet.create({
   header: {
-    paddingTop: Constants.statusBarHeight,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
+    borderRadius: 30,
+    paddingLeft: 10,
+
+  },
+  bar: {
+    flex: 5,
+    marginTop: 3,
+    backgroundColor: 'transparent',
+    width: 285,
+    paddingTop: 7,
+  },
+  button: {
+    flex: 1
   }
 })
--- Components/SearchScreen.js
+++ Components/SearchScreen.js
@@ -1,22 +1,59 @@
 import React, { Component } from 'react';
-import { Text,  Platform, StatusBar  } from 'react-native';
+import { FlatList, View, ActivityIndicator } from 'react-native';
 import SearchBarComponent from './SearchBarComponent';
 import { StackNavigator } from 'react-navigation';
+import MovieItem from './MovieItem';
 
 export default class SearchScreen extends Component {
-  static navigationOptions = () => {
+  constructor(props) {
+    super(props);
+    this.state ={
+      movieResults: [],
+      loading: false
+    }
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  static navigationOptions = ({navigation}) => {
     return {
-      header: (
-        <SearchBarComponent />
-      ),
-      headerStyle: {
-        paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
-      }
+      headerRight:
+        <SearchBarComponent searchText={navigation.getParam('handleSearch')}/>
     }
   };
+
+  componentWillMount() {
+    this.props.navigation.setParams({ handleSearch: this.handleSearch });
+  }
+
+  async handleSearch(text) {
+    this.setState({
+      loading: true,
+      movieResults: []
+    });
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&query=${text}`;
+    let response = await fetch(url);
+    let result = await response.json();
+    this.setState({
+      loading: false,
+      movieResults: result.results
+    })
+  }
+
   render() {
     return (
-      <Text>Search something here!</Text>
-    )
+      <View style={{flex: 1}}>
+        {this.state.loading ? (
+          <View>
+            <ActivityIndicator size="large" />
+          </View>
+        ) : (
+          <FlatList style={{elevation: 5, flex: 1}}
+            overScrollMode= 'always'
+            data={this.state.movieResults}
+            renderItem={({item}) => {return (<MovieItem {...item}></MovieItem>)} }
+          />
+        )}
+      </View>
+    );
   }
 }
